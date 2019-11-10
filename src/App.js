9f6a277cd0b586eca377f86dc11e7d93749e1142
github username <kenlyn.terai@gmail.com>
2019-11-10 00:03:32 -0800

    Add state hooks and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [githubUsername, setGithubUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const url = `https://api.github.com/users/${githubUsername}`
+    fetch(url, {
+    })
+        .then(response => response.json())
+        .then(user => setUser(user));
+    }
+
   return (
     <div className="App">
-      
+       <form onSubmit={event => handleSubmit(event)}>
+              <h2>Enter a Github username:</h2>
+              <input type="text" name="githubUsername" onChange={event => setGithubUsername(event.target.value)} value={githubUsername} />
+          </form>
     </div>
   );
 }
