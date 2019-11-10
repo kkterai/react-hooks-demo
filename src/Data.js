119f366ad44ead2a6a9d9357ceb7e529585190ed
github username <kenlyn.terai@gmail.com>
2019-11-10 00:40:47 -0800

    Remove whitespace

--- src/Data.js
+++ src/Data.js
@@ -5,9 +5,7 @@ const Data = (props) =>
     [<div>
         <br></br>
         { props.userInfo.name ? <h1><b>{props.userInfo.name}</b></h1> : "" }
-        
         { props.userInfo.avatar_url ? <img src= {props.userInfo.avatar_url} alt="avatar" ></img> : "" }
-
         <h2><a href={props.userInfo.html_url}>{props.userInfo.login}</a></h2>
         { props.userInfo ? <p><a href={props.userInfo.repos_url}>{props.userInfo.public_repos} public repos</a></p> : "" }
         { props.userInfo.email ? <p>Email: {props.userInfo.email}</p> : "" }
